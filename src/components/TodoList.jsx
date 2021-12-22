4d937db8f2bf1f1f1a30b66fe6474b7fd6737b0e
sanjeevi3 <sanjeevi.developer@gmail.com>
2021-12-22 15:23:30 +0530

    ui changes

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,10 +9,12 @@ const TodoList = props=>{
             <div className="mb-3">
                 <h3>TODO LIST</h3>
             </div>
+            
             {
-                props.todoList.map((todo, index)=>{
+                props.todoList.length?props.todoList.map((todo, index)=>{
                    return <Todo key={index} id ={index} todo = {todo} />
-                })
+                }):
+                <div className="text-danger font-weight-bold text-capitalize text center">todo was not found.</div>
             }
         </div>
         </div>
